b34f93bf36b5d9a0e6537c4684638ed117b7b2d0
InduRani11 <pavitramindu999@gmail.com>
2026-07-14 10:30:11 +0530

    route handler and middleware

--- src/app.js
+++ src/app.js
@@ -1,28 +1,20 @@
 const express =require('express');
 const app = express();
-// "/" is root route it overrides all other routes if it is placed before them
-// app.use("/",(req,res)=>{
-//     res.send('Hello World');
-// })
-// app.use("/hello",(req,res)=>{
-//     res.send('Hello hello hello');
-// })
 
-app.get("/test",(req,res)=>{
-    // if you request on /test?userid=123  then you can access the query parameter using req.query.userid
-    console.log(req.query.userid)
-    res.send('Hello tests get request');
-})
-
-// this is dynamic route where you can pass parameter in the url like /test/123 and you can access the parameter using req.params.userid
-app.get("/test/:userid",(req,res)=>{
-     console.log(req.params.userid)
-    res.send('Hello tests get request');
-})
-app.get("/abc",(req,res)=>{
-    res.send('Hello tests get request');
-})
-
+app.use("/user",
+    // middleware
+    (req,res,next)=>{
+        console.log("Route 1 ")
+        next();
+        
+    },
+    // route handler
+    (req,res,next)=>{
+        console.log("Route 2 ")
+        // next(); 
+        res.send("User route 2");
+    }
+)
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
