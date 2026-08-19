15594a1395d958d68cc0c8cc4c16d52df35cc3a0
InduRani11 <pavitramindu999@gmail.com>
2026-08-19 10:07:13 +0530

    "login api or authantication"

--- src/app.js
+++ src/app.js
@@ -3,24 +3,48 @@ const {conectDB}=require('./config/database');
 const app = express();
 const User = require('./models/user');
 const {validateSignupData}=require("./utils/validation");
+const bycrypt = require('bcrypt');
 app.use(express.json());
+
 
 app.post('/signup',async (req,res)=>{
     // validate the request body
     try{
         validateSignupData(req);
-        const userobj=req.body;
-        console.log(userobj);
-        // creating a new instance of user model
-        const user=new User(userobj);
+        const {firstName,lastName,emailId,password}=req.body;
+        const passwordHash=await bycrypt.hash(password,10);
 
-    
+        const user=new User({
+            firstName,
+            lastName,
+            emailId,
+            password:passwordHash
+        });
+
         await user.save();
         res.send('User created successfully');
     }catch(err){
         res.status(500).send('Error creating user:'+err.message);
     }
     
+});
+
+app.post('/login',async (req,res)=>{
+    try{
+        const {emailId,password}=req.body;
+        const user=await User.findOne({emailId});
+        if(!user){
+            return res.status(401).send('Invalid email or password');
+        }
+        const isPasswordValid= await bycrypt.compare(password,user.password);
+        if(!isPasswordValid){
+            return res.status(401).send('Invalid email or password');
+        }else{
+            res.send('User logged in successfully');
+        }
+    }catch(err){
+        res.status(500).send('Error logging in user:'+err.message);
+    }
 });
 
 // get user by email
